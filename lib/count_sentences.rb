eaba144dbdf2aa023130f08ba549ae9956046658
Joshua Needham <joshuabneedham@gmail.com>
2017-10-31 00:21:20 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    self.end_with?(".")
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
